fcfd24fe7b3784d8b064b8901ed8b9376e198d07
Bagus Anugrah <bagus.anugrah71@gmail.com>
2025-01-15 17:56:54 +0700

    first commit

---
+++ flutter/lib/features/admin/widgets/category_products_chart.dart
@@ -0,0 +1,61 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:amazon_clone_tutorial/features/admin/models/sales.dart';
+
+class CategoryProductsChart extends StatelessWidget {
+  final List<Sales> salesData;
+
+  const CategoryProductsChart({
+    Key? key,
+    required this.salesData,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        barGroups: salesData
+            .asMap()
+            .entries
+            .map(
+              (entry) => BarChartGroupData(
+                x: entry.key,
+                barRods: [
+                  BarChartRodData(
+                    fromY: 0,
+                    toY: entry.value.earning.toDouble(),
+                    color: Colors.blue, // Menggunakan parameter 'color'
+                  ),
+                ],
+              ),
+            )
+            .toList(),
+        titlesData: FlTitlesData(
+          leftTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              reservedSize: 40,
+            ),
+          ),
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                int index = value.toInt();
+                if (index >= 0 && index < salesData.length) {
+                  return Text(
+                    salesData[index].label,
+                    style: const TextStyle(fontSize: 12),
+                  );
+                }
+                return const Text('');
+              },
+              reservedSize: 40,
+            ),
+          ),
+        ),
+        borderData: FlBorderData(show: false),
+      ),
+    );
+  }
+}
